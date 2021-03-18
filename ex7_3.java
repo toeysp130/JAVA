486ba3c969c3fe79fe114a9bd39df0c28d4f578d
toeysp130 <choptum@hotmail.co.th>
2021-03-18 19:58:21 +0700

    update

--- ex7_3.java
+++ ex7_3.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 public class ex7_3 implements ActionListener {
     JLabel numberLabel;
     JTextField numberField;
-    JButton[] btn = new JButton[10];
+    JButton[] btn = new JButton[10] ;
+    JButton btn_mod , btn_ce , btn_c , btn_backspace;
     JFrame window;
     public ex7_3() {
         // obtain content pane and set its layout to FlowLayout
@@ -17,12 +18,32 @@ public class ex7_3 implements ActionListener {
         numberField = new JTextField( 10 );
         //numberField.setEditable( false );
         container.add( numberField );
+        btn_mod = new JButton("%");
+        btn_mod.addActionListener( this);
+        container.add(btn_mod);
+
+        btn_ce = new JButton("CE");
+        btn_ce.addActionListener( this);
+        container.add(btn_ce);
+
+        btn_c = new JButton("C");
+        btn_c.addActionListener( this);
+        container.add(btn_c);
+
+        btn_backspace = new JButton("<--");
+        btn_backspace.addActionListener( this);
+        container.add(btn_backspace);
+
+        
+
+        //for นี้สร้างเลข
         for(int i = 0 ; i < 10 ;i++){
             String name =String.valueOf(i); 
             btn[i] = new JButton(name);
             btn[i].addActionListener( this);
             container.add( btn[i] );
         }
+        //จบ for สร้างเลข
         
         
         window.setSize( 250,300);
