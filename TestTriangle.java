4e41476fb93fae1319f752599d069bfb407e9d10
toeysp130 <choptum@hotmail.co.th>
2021-05-23 11:41:14 +0700

    commit

---
+++ TestTriangle.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+public class TestTriangle {
+    public static void main(String[] args) {
+    Triangle t = new Triangle(10,10);
+        System.out.print(t.toString());
+    }
+
+}
